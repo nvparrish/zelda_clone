93413245a2e49a878835ba8cc0bedf6af348f503
Nathan Parrish <nvparrish@gatech.edu>
2022-02-13 22:38:02 -0500

    Draws one character frame
    This is set up to draw one frame of the character to  test drawing a
     texture.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 os.environ["RAYLIB_BIN_PATH"] = "ext/raylib-2.0.0-Linux-amd64/lib/"
 #os.environ["RAYLIB_BIN_PATH"] = "/usr/local/lib/" # Possible if 2.0.0 is system version 
 
-import raylibpy as rl
+import pyray as rl
 import debug
 import settings
 
@@ -17,11 +17,19 @@ class Game:
     
     def run(self):
         # Main loop
+        CHARACTER = rl.load_texture('gfx/character.png')
         while not rl.window_should_close():
             rl.begin_drawing()
             rl.clear_background(rl.BLACK)
             rl.draw_text("Congrats!  You created your first window!", 190, 200, 20, rl.LIGHTGRAY)
             debug.debug(self.debug_info)
+            # rl.draw_texture(CHARACTER, 0, 0, rl.RAYWHITE)
+            rl.draw_texture_rec(
+                CHARACTER,
+                rl.Rectangle(0, 0, 16, 32),
+                rl.Vector2(20.0, 20.0),
+                rl.RAYWHITE
+            )
             rl.end_drawing()
 
         # clean up
